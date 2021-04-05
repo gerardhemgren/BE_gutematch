54ac741e739e487f54b4266e6614c52d94259a0c
gerard hemgren <gerardhemgren@gmail.com>
2021-04-04 21:13:05 -0300

    change Client to a separate Pool connection

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
-const { Client } = require('pg')
-
-const client = new Client({
-    user: 'postgres',
-    password: 'postre',
-    host: '127.0.0.1',
-    port: 5432,
-    database: 'FTBL'
-})
+const express = require('express')
+const app = express()
+const pool = require('./db')
 
 start()
 
@@ -14,18 +8,18 @@ const player_id = 4
 
 async function start() {
     try {
-        await client.connect()
-        console.log(`connected on PORT: ${client.port}
+        await pool.connect()
+        console.log(`connected on PORT: ${pool.port}
 
 `)
-        const name = await client.query(`SELECT name FROM players WHERE id = ${player_id}`)
+        const name = await pool.query(`SELECT name FROM players WHERE id = ${player_id}`)
         console.log(`Player: ${name.rows[0].name}
 
 JOINED GAMES:`)
-        const {rows} = await client.query(`SELECT * FROM show_player_games($1)`, [player_id])
+        const {rows} = await pool.query(`SELECT * FROM show_player_games($1)`, [player_id])
         console.table(rows)
         console.log('OPEN EVENTS TO JOIN:')
-        const eventRows = await client.query(`SELECT * FROM show_player_open_games($1)`, [player_id])
+        const eventRows = await pool.query(`SELECT * FROM show_player_open_games($1)`, [player_id])
         console.table(eventRows.rows)
 
     }
@@ -33,35 +27,8 @@ JOINED GAMES:`)
         console.log('Unnable: ', error)
     }
     finally {
-        await client.end()
+        await pool.end()
         console.log(`
 disconnected`)
     }
 }
-
-// async function start() {
-//     try {
-//         await client.connect()
-//         console.log(`connected on PORT: ${client.port}`)
-//         await client.query('BEGIN')
-//         await client.query('UPDATE players SET name = $1', ['jojo'])
-//         const {rows} = await client.query(`SELECT * FROM players`)
-//         console.table(rows)
-//         await client.query('CALL insert_player_update_game($1, $2)', [11,5])
-//         await client.query(`SELECT * FROM events`)
-//         console.table(rows)
-//         await client.query('COMMIT')
-
-//     }
-//     catch (error) {
-//         console.log('Unnable: ', error)
-//         await client.query('ROLLBACK')
-
-//     }
-//     finally {
-//         const {rows} = await client.query(`SELECT * FROM players`)
-//         console.table(rows)
-//         await client.end()
-//         console.log('disconnected')
-//     }
-// }
