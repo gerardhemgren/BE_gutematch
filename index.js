d109ee66eb86c3c29e2979936e00008670188ecb
gerard hemgren <gerardhemgren@gmail.com>
2021-04-07 18:46:31 -0300

    all api with response

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const pool = require('./db')
 
 app.use(express.json())
 
-app.get('/', async(req, res) => {
+app.get('/', async (req, res) => {
     try {
         res.json('Futbol App')
     } catch (error) {
@@ -13,7 +13,7 @@ app.get('/', async(req, res) => {
     }
 })
 
-app.get('/allmatchs', async(req, res) => {
+app.get('/allmatchs', async (req, res) => {
     try {
         const result = await pool.query(`SELECT * FROM games ORDER BY date`)
         res.json(result.rows)
@@ -22,11 +22,29 @@ app.get('/allmatchs', async(req, res) => {
     }
 })
 
-app.get('/myopengames/:id', async(req, res) => {
-    const {id} = req.params
+app.get('/mygames/:id', async (req, res) => {
+    const { id } = req.params
+    try {
+        const eventRows = await pool.query(`SELECT * FROM show_player_games($1)`, [id])
+        res.json(eventRows.rows)
+    } catch (error) {
+        res.redirect(`http://localhost:5000`)
+    }
+})
+
+app.get('/myopengames/:id', async (req, res) => {
+    const { id } = req.params
     try {
         const eventRows = await pool.query(`SELECT * FROM show_player_open_games($1)`, [id])
         res.json(eventRows.rows)
+    } catch (error) {
+        res.redirect(`http://localhost:5000`)
+    }
+})
+
+app.get('*', async (req, res) => {
+    try {
+        res.json('wrong request')
     } catch (error) {
         console.log(error.message)
     }
