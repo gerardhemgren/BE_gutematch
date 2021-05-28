2a9042b207a186637d916fb2465fc68fabba2793
gerard hemgren <gerardhemgren@gmail.com>
2021-05-28 12:01:25 -0300

    update PORT at the listen method

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.static('build'))
 app.use('/', match, user)
 
 
-const PORT = process.env.PORT || 3001
-app.listen(5000, () => {
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => {
     console.log('Server running on port 5000')
 })
