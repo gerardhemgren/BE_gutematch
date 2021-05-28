b5f619c307fcca2235984930be6d9fda24dfb5f1
gerard hemgren <gerardhemgren@gmail.com>
2021-05-28 15:28:08 -0300

    sslmode without ternary

--- utils/config.js
+++ utils/config.js
@@ -12,18 +12,15 @@ const devConfig = {
     port: process.env.PG_PORT,
     database: process.env.PG_DB
 }
+
 const prodConfig = {
-    connectionString: `${process.env.DATABASE_URL}`,
+    connectionString: `${process.env.URI}`,
     ssl: {
         rejectUnauthorized: false
     },
-    sslmode: process.env.NODE_ENV === "production" ? "require" : "disable"
+    sslmode: "require"
 }
 
 const pool = new Pool(prodConfig)
-
-/* pool.on("connect", client => {
-    client.on("notice", msg => console.log('config', msg.message))
-    }); */
 
 module.exports = { pool }
