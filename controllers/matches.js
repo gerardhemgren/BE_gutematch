57467162c770d40c88677ba80212432351dc44ff
gerard hemgren <gerardhemgren@gmail.com>
2022-04-20 14:04:21 -0300

    clientDate param from UI added

--- controllers/matches.js
+++ controllers/matches.js
@@ -1,104 +1,106 @@
-const express = require('express')
-const { pool } = require('../utils/config')
-const match = express()
+const express = require('express');
+const { pool } = require('../utils/config');
+const match = express();
 
 match.get('/', async (req, res) => {
     try {
-        res.json('Welcome')
+        res.json('Welcome');
     } catch (error) {
-        console.log(error.message)
+        console.log(error.message);
     }
 })
 
 match.get('/api/all_matches', async (req, res) => {
     try {
-        const result = await pool.query(`SELECT * FROM all_matches WHERE date >= current_timestamp ORDER BY date`)
-        res.json(result.rows)
+        const result = await pool.query(`SELECT * FROM all_matches WHERE date >= current_timestamp ORDER BY date`);
+        res.json(result.rows);
     } catch (error) {
-        console.log(error.message)
+        console.log(error.message);
     }
 })
 
 // Get my matches
-match.get('/api/my_matches/:id', async (req, res) => {
-    const { id } = req.params
+match.post('/api/my_matches/:id', async (req, res) => {
+    const { id } = req.params;
+    const { clientDate } = req.body;
     try {
-        const result = await pool.query(`SELECT * FROM my_matches($1)`, [id])
-        res.json(result.rows)
+        const result = await pool.query(`SELECT * FROM my_matches($1, $2)`, [id, clientDate]);
+        res.json(result.rows);
     } catch (error) {
-        res.json(error.message)
+        res.json(error.message);
     }
 })
 
 // Get open matches
-match.get('/api/open_matches/:id', async (req, res) => {
-    const { id } = req.params
+match.post('/api/open_matches/:id', async (req, res) => {
+    const { id } = req.params;
+    const { clientDate } = req.body;
     try {
-        const result = await pool.query(`SELECT * FROM open_matches($1)`, [id])
-        res.json(result.rows)
+        const result = await pool.query(`SELECT * FROM open_matches($1, $2)`, [id, clientDate]);
+        res.json(result.rows);
     } catch (error) {
-        res.json(error.message)
+        res.json(error.message);
     }
 })
 
 // Join match
 match.post('/api/open_matches/:id', async (req, res) => {
-    const { id } = req.params
-    const { id_match } = req.body
+    const { id } = req.params;
+    const { id_match } = req.body;
     try {
-        await pool.query(`CALL join_match($1, $2)`, [id_match, id])
-        res.json(`Match joined succesfully`)
+        await pool.query(`CALL join_match($1, $2)`, [id_match, id]);
+        res.json(`Match joined succesfully`);
     } catch (error) {
-        res.json(error.message)
+        res.json(error.message);
     }
 })
 
 // Leave match
 match.delete('/api/my_matches/:id', async (req, res) => {
-    const { id } = req.params
-    const { id_match } = req.body
+    const { id } = req.params;
+    const { id_match } = req.body;
     try {
-        await pool.query(`CALL leave_match($1, $2)`, [id_match, id])
-        res.json(`Match lefted succesfully`)
+        await pool.query(`CALL leave_match($1, $2)`, [id_match, id]);
+        res.json(`Match lefted succesfully`);
     } catch (error) {
-        res.json(error.message)
+        res.json(error.message);
     }
 })
 
 // Create match
 match.post('/api/create_match/:id', async (req, res) => {
-    const { id } = req.params
-    const { date, location, players_field, name } = req.body
+    const { id } = req.params;
+    const { date, location, players_field, name } = req.body;
     try {
         await
             pool.query(`SELECT create_match($1, $2, $3, $4, $5)`, [id, date, location, players_field, name])
-                .then(msg => res.json(msg.rows[0].create_match))
+                .then(msg => res.json(msg.rows[0].create_match));
     } catch (error) {
-        res.json(error.message)
+        res.json(error.message);
     }
 })
 
 // Delete my match
 match.delete('/api/my_matches/owner/:id', async (req, res) => {
-    const { id } = req.params
-    const { id_match } = req.body
+    const { id } = req.params;
+    const { id_match } = req.body;
     try {
         await pool.query(`SELECT delete_match($1, $2)`, [id_match, id])
-            .then(msg => res.json(msg.rows[0].delete_match))
+            .then(msg => res.json(msg.rows[0].delete_match));
     } catch (error) {
-        res.json(error.message)
+        res.json(error.message);
     }
 })
 
 
 // Edit match
 match.put('/api/my_matches/owner/', async (req, res) => {
-    const { matchId, date, location, players_field, name } = req.body
+    const { matchId, date, location, players_field, name } = req.body;
     try {
         await pool.query(`SELECT edit_match($1, $2, $3, $4, $5)`, [matchId, date, location, players_field, name])
-            .then(msg => res.json(msg.rows[0].edit_match))
+            .then(msg => res.json(msg.rows[0].edit_match));
     } catch (error) {
-        res.json(error.message)
+        res.json(error.message);
     }
 })
 
@@ -106,10 +108,10 @@ match.put('/api/my_matches/owner/', async (req, res) => {
 // Handle wrong requests
 match.get('*', async (req, res) => {
     try {
-        res.json('Wrong request')
+        res.json('Wrong request');
     } catch (error) {
-        console.log(error.message)
+        console.log(error.message);
     }
 })
 
-module.exports = match
+module.exports = match;
